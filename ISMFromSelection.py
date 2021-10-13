4ca7b880d2ad333f3a5e549990f1f1d7c3a4be7b
nicolas santamaria <nicosanta@gmail.com>
2021-10-13 20:33:52 +0200

    Add files via upload
    add -h args for in-script documentation
    change -e to -d for deletion
    add -p to pass class Prototype (MustContain InstancedStaticMeshComponent)

--- ISMFromSelection.py
+++ ISMFromSelection.py
@@ -7,31 +7,21 @@ license: LGPL
 
 import unreal
 import sys
-from itertools import groupby
 # https://docs.unrealengine.com/4.27/en-US/PythonAPI/
 # https://sondreutheim.com/post/getting_started_with_python_in_ue4
 # https://www.youtube.com/watch?v=UGnbx7iNMBQ&list=PLBLmKCAjA25Br8cOVzUroqi_Nwipg-IdP
-
-
 # https://answers.unrealengine.com/questions/904813/python-attach-component-to-actor-or-blueunreal.log.html
 
 class_to_load = '/Game/BP_InstancedStaticMeshActor.BP_InstancedStaticMeshActor'
-props_to_copy = ['static_mesh', 'override_materials', 'cast_shadow', 'mobility']
 
 def help():
-	print("Instantiate.py\n\n\
+	print("ISMFromSelection.py\n\n\
 	short:\n\
-		Select StaticMeshActors in Level and create a InstancedStaticMeshActor with all transforms of the selection.\n\
-		Multiple StaticMesh results in multiple InstancedStaticMeshActor\n\
+		Select Actors in Level and create a InstancedStaticMeshActor with all transforms of the selection\n\
 	args:\n\
-		-d		Delete StaticMeshActors after creation\n\
 		-p		Define Prototype of the actor type (ex: Blueprint'/Game/BP_InstancedStaticMeshActor.BP_InstancedStaticMeshActor')\n\
 		-h		Print this help\n\
 	")
-
-def copyProps(dest, src, props):
-	for p in props:
-		dest.set_editor_property(p, src.get_editor_property(p))
 
 def spawnHISM(actors):
 	# actor_instance = unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.Actor)
@@ -48,11 +38,6 @@ def spawnHISM(actors):
 			if instance_component:
 				for sm in actors:
 					instance_component.add_instance_world_space(sm.get_actor_transform())
-				first_mesh_component = actors[0].static_mesh_component
-
-				# Copy properties
-				copyProps(instance_component, first_mesh_component, props_to_copy)
-
 			return actor_instance
 		except Exception as err:
 			unreal.log_error("Error during ISM Actor creation: \n\t %s" % err)
@@ -74,31 +59,17 @@ def getSelectedActors(actor_class = None):
 		return class_actors
 	return selection
 
-
 def execute():
-	selected_static_mesh = getSelectedActors(unreal.StaticMeshActor)
-	selected_static_mesh.sort(key= lambda x: x.static_mesh_component.static_mesh.get_path_name())
+	selected_actors = unreal.EditorLevelLibrary.get_selected_level_actors()
 
-	for x in selected_static_mesh:
-		path = x.static_mesh_component.static_mesh.get_path_name()
-
-	for key, group in groupby(selected_static_mesh, lambda x: x.static_mesh_component.static_mesh.get_path_name()):
-		items = list(group)
-		# unreal.log(f'groups {key} with {len(items)} item(s)')
-		# unreal.log(f'item(s): {items}')
-
-		hism_actor = spawnHISM(items)
-		if hism_actor:
-			hism_component = hism_actor.get_component_by_class(unreal.InstancedStaticMeshComponent)
-			unreal.log('Spawned ISM for mesh %s with %d instances' % (key, hism_component.get_instance_count()))
-		else:
-			unreal.log_error("Error during generation")
-	if '-d' in sys.argv:
-		for x in selected_static_mesh:
-			unreal.EditorLevelLibrary.destroy_actor(x)
+	hism_actor = spawnHISM(selected_actors)
+	if hism_actor:
+		hism_component = hism_actor.get_component_by_class(unreal.InstancedStaticMeshComponent)
+		unreal.log('Spawned ISM with %d instances' % hism_component.get_instance_count())
 	else:
-		unreal.EditorLevelLibrary.set_selected_level_actors(selected_static_mesh)
-	
+		unreal.log_error("Error during generation")
+	unreal.EditorLevelLibrary.set_selected_level_actors(selected_actors)
+
 try:
 	if '-p' in sys.argv:
 		pindex = sys.argv.index('-p')
@@ -108,5 +79,6 @@ try:
 		help()
 	else:
 		execute()
-except:
+except Exception as err:
+	unreal.log_error(err)
 	help()
